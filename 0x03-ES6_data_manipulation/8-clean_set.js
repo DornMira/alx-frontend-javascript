abfe9cad0fdcf781d991b2685cdcc96c2259d696
DornMira <111016349+DornMira@users.noreply.github.com>
2023-07-04 16:03:27 -0400

    Update 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,25 +1,14 @@
-/* eslint-disable linebreak-style */
-/* eslint-disable no-unused-vars */
-const cleanSet = (aSet, startString) => {
-  const arr = Array.from(aSet);
-  const newArr = [];
+export default function cleanSet(set, startString) {
+  let text = '';
+  const array = [];
 
-  if (startString.length === 0) {
-    return '';
-  }
-
-  for (const i of arr) {
-    const str = i.slice(0, startString.length);
-
-    if (str === startString) {
-      newArr.push(i);
+  if (startString && typeof startString === 'string') {
+    for (const element of set) {
+      if (element && element.startsWith(startString)) {
+        array.push(element.slice(startString.length));
+      }
     }
+    text = array.join('-');
   }
-
-  return newArr.toString().split(',').join('-').split(startString)
-    .toString()
-    .split(',')
-    .join('');
-};
-
-export default cleanSet;
+  return text;
+}
